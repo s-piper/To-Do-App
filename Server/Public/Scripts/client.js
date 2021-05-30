9c15adf3892a67e0a750c226d452efc0baf31c34
Stephen Piper <stephen.r.piper@gmail.com>
2021-05-29 19:04:59 -0500

    finished click listener and client side POST.

--- Server/Public/Scripts/client.js
+++ Server/Public/Scripts/client.js
@@ -7,6 +7,32 @@ $( document ).ready( function(){
   
   }); // end jQuery
   
+
+  //starts the function for button clicks
+  //collects input data
   function clickListeners() {
-      
+      $('#addBtn').on('click', function() {
+          console.log('in add btn click');
+          
+          let taskToSend = {
+              details: $('#taskIn').val(),
+              priority: $('#priority'),val()
+          };
+
+          sendTask(taskToSend);
+      })
+  }
+
+
+  function sendTask(newTask) {
+    $.ajax({
+        method:'POST',
+        url: '/todo',
+        data: newTask
+      }).then(response => {
+        console.log('server says:', response);
+        loadTasks();
+      }).catch(err =>{
+        alert('Not sure how to tell you this:', err);
+      })
   }
